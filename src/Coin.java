54915042379803ad9c37e0c84c42d2fead45a2fd
AndyGuttermanLaptop <Andygutterman@outlook.com>
2023-10-16 21:43:54 -0400

    added stuff

--- src/Coin.java
+++ src/Coin.java
@@ -1,8 +1,17 @@
 public class Coin {
-    private static String Name;
+    private static String name;
     private static float value;
     private int roll_limit;
-    public Coin(){
 
-    };
+    public int roll_value = (int) (roll_limit * value);
+
+    public Coin(String name, float val, int roll_limit) {
+        this.name = name;
+        this.value = val;
+        this.roll_limit = roll_limit;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/CoinCounter.java
+++ src/CoinCounter.java
@@ -1,6 +1,31 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import javax.swing.*;
-public class CoinCounter {
 
+
+public class CoinCounter{
+    JFrame mainWindow = new JFrame();
+
+
+
+    public CoinCounter(){
+        ArrayList<Coin> CoinList = new ArrayList();
+        Coin Quarter = new Coin("Quarter", 0.25F, 40);
+        CoinList.add(Quarter);
+
+        JPanel coinPanel = new JPanel();
+
+        for (Coin coin: CoinList){
+            JLabel coinNameLabel = new JLabel(coin.getName());
+            coinPanel.add(coinNameLabel);
+            JTextField coin_textfield = new JTextField();
+        }
+    };
+
+    public static void main(String[] args) {
+
+    }
 }
+
