956dcfc7fcb8db133b83646d3a8297b708b9dc3d
AndyGuttermanLaptop <Andygutterman@outlook.com>
2023-10-17 08:07:56 -0400

    made coin variables not static so they can be changed for different coin types

--- src/Coin.java
+++ src/Coin.java
@@ -1,14 +1,14 @@
 public class Coin {
-    private static String name;
-    private static float value;
+    private String name; // Change from static to non-static
+    private float value; // Change from static to non-static
     private int roll_limit;
-
-    public int roll_value = (int) (roll_limit * value);
+    public int roll_value; // Also, make this non-static
 
     public Coin(String name, float val, int roll_limit) {
         this.name = name;
         this.value = val;
         this.roll_limit = roll_limit;
+        this.roll_value = (int) (roll_limit * val); // Initialize roll_value here
     }
 
     public String getName() {
--- src/CoinCounter.java
+++ src/CoinCounter.java
@@ -1,31 +1,49 @@
 import java.awt.*;
-import java.awt.event.*;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import javax.swing.*;
 
+public class CoinCounter {
+    private JFrame mainWindow;
 
-public class CoinCounter{
-    JFrame mainWindow = new JFrame();
+    public CoinCounter() {
+        mainWindow = new JFrame("Coin Counter");
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainWindow.setSize(400, 300);
 
-
-
-    public CoinCounter(){
-        ArrayList<Coin> CoinList = new ArrayList();
+        ArrayList<Coin> CoinList = new ArrayList<>();
         Coin Quarter = new Coin("Quarter", 0.25F, 40);
+        Coin Dime = new Coin("Dime", 0.10F, 50);
+        Coin Nickel = new Coin("Nickel", .05F, 50);
+
         CoinList.add(Quarter);
+        CoinList.add(Dime);
+        CoinList.add(Nickel);
 
-        JPanel coinPanel = new JPanel();
+        JPanel coinPanel = new JPanel(new GridBagLayout());
+        mainWindow.add(coinPanel);
 
-        for (Coin coin: CoinList){
-            JLabel coinNameLabel = new JLabel(coin.getName());
-            coinPanel.add(coinNameLabel);
-            JTextField coin_textfield = new JTextField();
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(5, 5, 5, 5); // Add padding
+
+        for (Coin coin : CoinList) {
+            JLabel coinNameLabel = new JLabel(coin.getName()+"s:");
+            JTextField coin_textfield = new JTextField(8); // Set the desired width
+
+            coinNameLabel.setName(coin.getName());
+            coin_textfield.setName(coin.getName());
+
+            coinPanel.add(coinNameLabel, gbc);
+            gbc.gridwidth = GridBagConstraints.REMAINDER; // New line
+            coinPanel.add(coin_textfield, gbc);
+            gbc.gridwidth = GridBagConstraints.RELATIVE; // Back to previous line
         }
-    };
+    }
 
     public static void main(String[] args) {
-
+        SwingUtilities.invokeLater(() -> {
+            CoinCounter coinCounter = new CoinCounter();
+            coinCounter.mainWindow.setVisible(true);
+        });
     }
 }
-
